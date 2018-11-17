20803afbcfda862a6cc421fd0f13f9659b30a1d8
kennedycAlves <kennedy.c.alves@gmail.com>
2018-11-16 22:54:59 -0200

    aqui coloco uma mensagem, tipo: comitando primeiro post

---
+++ exer03 - Copia.py
@@ -0,0 +1,9 @@
+a = int(input("Informe um numero: "))
+b = int(input("Informe outro numero"))
+
+if a > b:
+    print ("O primeiro numero informado é maior")
+
+if a < b:
+    print ("O segundo numero informado é maior")
+    
